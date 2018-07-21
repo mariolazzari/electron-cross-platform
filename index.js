9b94757f22eb57efda58d78a4f2443d23110fae6
mariolazzari <mario.lazzari@gmail.com>
2018-07-21 17:58:02 +0200

    Added once event

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const { BrowserWindow, app } = require("electron");
 let win;
 
 app.on("ready", () => {
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ width: 800, height: 600, show: false });
     win.loadURL("http://www.italcoeng.com");
 
     let infoWin = new BrowserWindow({
         width: 400,
         height: 300,
         transparent: true,
-        frame: false
+        frame: false,
+        show: false
     });
     infoWin.loadURL("file://" + __dirname + "/info.html");
+
+    win.once("ready-to-show", () => {
+        win.show();
+        infoWin.show();
+    });
 });
